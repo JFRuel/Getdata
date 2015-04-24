8e3ddcfc99e7a5f63d59cf2f50bbc8e071059204
JFRuel <jf@arlequinade.com>
2015-04-23 20:20:53 -0400

    Files from the course

---
+++ README.md
@@ -0,0 +1,53 @@
+==================================================================
+Getting and Cleaning Data - Coursera "getdata-013"
+Course Project  - April 2015
+==================================================================
+JFRuel on GitHub
+==================================================================
+
+This document explains how all the R script works and how it is connected to the input and output files.  
+
+To be able to run the R file, you will need to download the source datasets using the following URL:
+"Human Activity Recognition Using Smartphones Data Set".
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+
+## The folder should include the following files:
+=================================================
+
+- 'README.MD' : Description of steps and setup required for running the R code
+- 'CodeBook.MD' : Description of the input data, the transformations, and output data
+- 'run_analysis.R' : R code used to create the output dataset
+- 'features_info.txt': Shows information about the variables used on the feature vector.
+- 'features.txt': List of all features.
+- 'activity_labels.txt': Links the class labels with their activity name.
+- 'train/X_train.txt': Training set.
+- 'train/y_train.txt': Training labels.
+- 'test/X_test.txt': Test set.
+- 'test/y_test.txt': Test labels.
+- 'train/subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30. 
+
+## Running the code
+====================
+
+The R code was built and tested using 
+	R version 3.1.3 (2015-03-09) -- "Smooth Sidewalk"
+	Platform: i386-w64-mingw32/i386 (32-bit)
+	dplyr package version 0.4.1
+
+The "run_analysis.R" must be run from the working directory.
+The "UCI HAR" (see above) must be extracted in the working directory.
+
+The R code will use all of the above ".txt" files as input (including subfolders as describe above).
+The output dataset will be created in the working directory, as "Mean_by_subject_and_activity.txt"
+
+## License:
+============
+
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
---
+++ run_analysis.R
@@ -0,0 +1,68 @@
+## This code was built to answer Coursera Getdata-013 programming assignment
+## It assumes that the code is run from the working directory
+
+## Setting up the library
+        library(dplyr)
+  
+## Reads the files in data frames that can be used by dplyr
+        Number_rows <- 20000  # for testing purposes, can be set to a small number
+  
+        X_train <- tbl_df(read.table("train/X_train.txt", nrow=Number_rows))
+        Y_train <- tbl_df(read.table("train/Y_train.txt", nrow=Number_rows))
+        S_train <- tbl_df(read.table("train/Subject_train.txt", nrow=Number_rows))
+
+        X_test <- tbl_df(read.table("test/X_test.txt", nrow=Number_rows))
+        Y_test <- tbl_df(read.table("test/Y_test.txt", nrow=Number_rows))
+        S_test <- tbl_df(read.table("test/Subject_test.txt", nrow=Number_rows))
+  
+        features <- tbl_df(read.table("features.txt"))
+        features <- select(features,V2)
+        features <- t(features)
+              
+        labels <- tbl_df(read.table("activity_labels.txt"))
+  
+  
+## Create a single dataset with "train" and "test" data
+  
+        ## Changes the names of the variables for S_ & Y_, so that there is no duplicate
+        ## variable names
+        S_train <- rename(S_train,subject=V1)
+        Y_train <- rename(Y_train,Y1=V1)
+        S_test <- rename(S_test,subject=V1)
+        Y_test <- rename(Y_test,Y1=V1)
+        
+        ## Adds the columns names to the X_ files, as provided in the features.txt dataset 
+        names(X_test) <- features
+        names(X_train) <- features
+        
+        ## Bind rows of the test and train datasets
+        Y <- rbind(Y_train,Y_test)
+        X <- rbind(X_train,X_test)
+        S <- rbind(S_train,S_test)
+        
+        
+        ## Binds the columns of the resulting 3 dataset 
+        DF_complete <- cbind(S,Y,X)
+        
+        ## Add labels from activities to the merged file, using a left join
+        labels <- rename(labels,activity_label=V2)
+        DF_complete <- left_join(DF_complete,labels,by = c("Y1" = "V1"), copy = FALSE)
+
+## Extracts only the measurements on the mean and 
+## standard deviation for each measurement
+## [JFR] I read this as keeping all the non-measurement variables + measurement 
+## variables that provides mean and std
+
+        ## First we need to remove invalid characters in column names
+        valid_column_names <- make.names(names=names(DF_complete), unique=TRUE, allow_ = TRUE)
+        names(DF_complete) <- valid_column_names
+        
+        ## Now we keep only the 86 measurement columns + subject + activity columns
+        DF_mean_std <- select(DF_complete, matches("subject"),matches("activity_label"),contains("mean"),contains("std"))
+  
+## Creates a second, independent tidy data set with the average of 
+## each variable for each activity and each subject
+        DF_mean_std <- group_by(DF_mean_std,subject,activity_label)
+        DF_summarized <- summarise_each(DF_mean_std, funs(mean))
+        
+        write.table(DF_summarized,"Mean_by_subject_and_activity.txt", row.names=FALSE)
